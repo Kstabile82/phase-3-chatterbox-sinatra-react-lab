e45c003124fa74cd2512093ec58ba202083c03c6
KarinaStabile <karina.stabile@gmail.com>
2022-04-01 20:39:48 -0400

    Completed assignment

--- chatterbox-server/app/controllers/application_controller.rb
+++ chatterbox-server/app/controllers/application_controller.rb
@@ -1,6 +1,32 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
-  # Add routes
+  get '/messages' do 
+    messages = Message.all.order(:created_at)
+    messages.to_json
+  end
+
+  post '/messages' do 
+    mess = Message.create(
+      body: params[:body],
+      username: params[:username]
+    )
+    mess.to_json
+  end
+
+  patch '/messages/:id' do 
+    mess = Message.find(params[:id])
+    mess.update(
+      body: params[:body]
+      )
+    mess.to_json
+  end
+
+  delete '/messages/:id' do 
+    mess = Message.find(params[:id])
+    mess.destroy
+    mess.to_json
+  end
+
   
 end
